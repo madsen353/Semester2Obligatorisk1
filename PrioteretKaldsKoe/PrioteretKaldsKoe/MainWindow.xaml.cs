887a803ebfa3fcaef02789d11ea3498adc18ee01
Rasmus <rasm3600@student.eadania.dk>
2019-02-08 16:30:56 +0100

    Ja

---
+++ PrioteretKaldsKoe/PrioteretKaldsKoe/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrioteretKaldsKoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PriorityQueue<int> myQueue = new PriorityQueue<int>();
+        //List<int> callerID = new List<int>();
+        private string currentCallPickUpTimestamp;
+        private int currentCall;
+
+
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_TakeCall_Click(object sender, RoutedEventArgs e)
+        {
+            //if (currentCall == 0)
+            //{
+                //currentCall = myQueue.First();
+                myQueue.Dequeue();
+                currentCallPickUpTimestamp = DateTime.Now.ToString();
+           // }
+
+        }
+
+        private void btn_EndCall_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_MakeCall_Click(object sender, RoutedEventArgs e)
+        {
+
+            //callerID.Add(i++);
+            myQueue.Enqueue(callerID.Count);
+        }
+
+
+    }
+}
---
+++ PrioteretKaldsKoe/PrioteretKaldsKoe/PriorityQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrioteretKaldsKoe
+{
+    class PriorityQueue <T>
+    {
+
+            private T[] arrayOfGivenObjectType;
+            private int arrayCount = 1;
+            private int currentArraySpot = 0;
+
+
+
+            public PriorityQueue()
+            {
+                this.arrayOfGivenObjectType = new T[arrayCount];
+
+            }
+
+            public void QueueExpand()
+            {
+                T[] newArray = new T[arrayOfGivenObjectType.Length * 2];
+                for (int i = 0; i < arrayOfGivenObjectType.Length; i++)
+                    newArray[i] = arrayOfGivenObjectType[i];
+                arrayOfGivenObjectType = newArray;
+            }
+
+
+
+
+            public void Enqueue(T obj)
+            {
+
+                if (arrayCount == currentArraySpot + 1)
+                {
+
+                    QueueExpand();
+                    arrayOfGivenObjectType[currentArraySpot] = obj;
+                    arrayCount *= 2;
+                }
+                else
+                {
+
+                    arrayOfGivenObjectType[currentArraySpot] = obj;
+
+                }
+
+                currentArraySpot++;
+            }
+
+            public void Dequeue(T obj)
+            {
+            
+            arrayOfGivenObjectType = arrayOfGivenObjectType.Skip(1).ToArray();
+            currentArraySpot--;
+
+        }
+        }
+}
